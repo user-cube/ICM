188bb0885125c1e74201b4b048eb6744912821ba
Rui Coelho <ruicoelho@ua.pt>
2020-02-06 18:24:32 +0000

    Linear Layout

--- HelloToast/app/src/main/java/com/rc/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/rc/hellotoast/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.showCount);
+        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
     /**
