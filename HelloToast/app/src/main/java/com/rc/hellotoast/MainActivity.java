5294554684b98e506c85f064673178b4a6b4ef97
Rui Coelho <ruicoelho@ua.pt>
2020-02-06 17:40:12 +0000

    Tutorial 1 and 2

---
+++ HelloToast/app/src/main/java/com/rc/hellotoast/MainActivity.java
@@ -0,0 +1,57 @@
+package com.rc.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    /**
+     * Sets dark or light mode according to user changes.
+     *
+     * @param newConfig New configuration according to user changes.
+     */
+    public void onConfigurationChanged (Configuration newConfig){
+        int currentNightMode = newConfig.uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're using the light theme
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're using dark theme
+                break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.showCount);
+    }
+
+    /**
+     * Displays the Toast.
+     *
+     * @param view
+     */
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount ++;
+        if (mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
